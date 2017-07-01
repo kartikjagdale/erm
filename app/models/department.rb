9f8373954938c10020dc92ede73210a104b56796
voidzero <kartikjagdale11@gmail.com>
2017-07-01 18:01:34 +0530

    Add Department Model

---
+++ app/models/department.rb
@@ -0,0 +1,4 @@
+class Department < ApplicationRecord
+  validates :name, presence: true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701115401) do
+ActiveRecord::Schema.define(version: 20170701122650) do
+
+  create_table "departments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       default: "", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
